4b528987406fe65fa01b98930ae7cd7d1b8facc2
Dmitry Kostenko <60621984+dmitrykostenkoweb@users.noreply.github.com>
2023-04-01 17:05:32 +0200

    updates user models

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,19 +1,23 @@
 import { Document, model, Schema } from "mongoose";
 
-export interface IBooksCheckedOut extends Document {
+export interface ICheckedOut {
   book_id: string;
-  due_date: string;
+  lease_date: string;
 }
+const checkedOutSchema: Schema = new Schema({
+  book_id: { type: String, required: false },
+  lease_date: { type: String, required: false },
+});
 export interface IUser extends Document {
   name: string;
   email: string;
-  books_checked_out: IBooksCheckedOut[];
+  checked_out: ICheckedOut[];
 }
 
 const userSchema: Schema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  books_checked_out: { type: Array, required: false },
+  checked_out: { type: Array(checkedOutSchema), required: false },
 });
 
 export default model<IUser>("User", userSchema);
