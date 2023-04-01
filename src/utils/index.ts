a2c93116f2fbd2d77e452209c43866dd0ce4bd7e
Dmitry Kostenko <60621984+dmitrykostenkoweb@users.noreply.github.com>
2023-04-01 17:06:08 +0200

    create helpers

---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+export const generateCurrentDate = (): string => {
+  const currentDate = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  };
+  return currentDate.toLocaleDateString("pl-PL", options);
+};
