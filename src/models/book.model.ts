75e0943df54fa7ba429f727a3c61ca97745ee99a
Dmitry Kostenko <60621984+dmitrykostenkoweb@users.noreply.github.com>
2023-04-01 17:02:56 +0200

    updates book models

--- src/models/book.model.ts
+++ src/models/book.model.ts
@@ -4,12 +4,14 @@ export interface IBook extends Document {
   title: string;
   author: string;
   genre: string;
+  available: boolean;
 }
 
 const bookSchema: Schema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
   genre: { type: String, required: true },
+  available: { type: Boolean, required: true },
 });
 
 export default model<IBook>("Book", bookSchema);
