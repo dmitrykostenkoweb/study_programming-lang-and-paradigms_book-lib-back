c3fc5d1dc08f4c33cc04024b576c988235a22aa2
Dmitry Kostenko <60621984+dmitrykostenkoweb@users.noreply.github.com>
2023-04-01 17:02:26 +0200

    updates type in book controller

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,39 +1,39 @@
-import express, { Request, Response } from "express";
-import Book from "../models/book.model";
+import express, { Request, Response, Router } from "express";
+import Book, { IBook } from "../models/book.model";
 
 class BookController {
-  private path = "/books";
-  public router = express.Router();
+  private path: string = "/books";
+  public router: Router = express.Router();
 
   constructor() {
     this.initializeRoutes();
   }
 
-  private initializeRoutes() {
+  private initializeRoutes(): void {
     this.router.get(this.path, this.getBooks);
     this.router.post(this.path, this.createBook);
     this.router.get(`${this.path}/:id`, this.getBookById);
     this.router.put(`${this.path}/:id`, this.updateBook);
     this.router.delete(`${this.path}/:id`, this.deleteBook);
   }
-
-  private async getBooks(req: Request, res: Response) {
+  private async getBooks(req: Request, res: Response): Promise<void> {
     try {
       const { title, author, genre } = req.query;
       const query: { [key: string]: any } = {};
       if (title) query["title"] = title;
       if (author) query["author"] = author;
       if (genre) query["genre"] = genre;
-      const books = await Book.find(query);
+
+      const books: IBook[] = await Book.find(query);
       res.send(books);
     } catch (error) {
       res.status(500).send(error);
     }
   }
 
-  private async createBook(req: Request, res: Response) {
+  private async createBook(req: Request, res: Response): Promise<void> {
     try {
-      const book = new Book(req.body);
+      const book: IBook = new Book(req.body);
       await book.save();
       res.send(book);
     } catch (error) {
@@ -41,38 +41,46 @@ class BookController {
     }
   }
 
-  private async getBookById(req: Request, res: Response) {
+  private async getBookById(
+    req: Request,
+    res: Response
+  ): Promise<Response<IBook> | undefined> {
     try {
-      const book = await Book.findById(req.params.id);
-      if (!book) {
-        return res.status(404).send("Book not found");
-      }
+      const book: IBook | null = await Book.findById(req.params.id);
+      if (!book) return res.status(404).send("Book not found");
+
       res.send(book);
     } catch (error) {
       res.status(500).send(error);
     }
   }
 
-  private async updateBook(req: Request, res: Response) {
+  private async updateBook(
+    req: Request,
+    res: Response
+  ): Promise<Response<IBook> | undefined> {
     try {
-      const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      const { params, body } = req;
+      const book: IBook | null = await Book.findByIdAndUpdate(params.id, body, {
         new: true,
       });
-      if (!book) {
-        return res.status(404).send("Book not found");
-      }
+
+      if (!book) return res.status(404).send("Book not found");
+
       res.send(book);
     } catch (error) {
       res.status(500).send(error);
     }
   }
 
-  private async deleteBook(req: Request, res: Response) {
+  private async deleteBook(
+    req: Request,
+    res: Response
+  ): Promise<Response<IBook> | undefined> {
     try {
-      const book = await Book.findByIdAndDelete(req.params.id);
-      if (!book) {
-        return res.status(404).send("Book not found");
-      }
+      const book: IBook | null = await Book.findByIdAndDelete(req.params.id);
+      if (!book) return res.status(404).send("Book not found");
+
       res.send(book);
     } catch (error) {
       res.status(500).send(error);
